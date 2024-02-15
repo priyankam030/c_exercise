648f0d9d4ddb5a68c23da2ec948bfd31fc550e41
priyankam030 <priyankamourya030@gmail.com>
2024-02-15 20:48:47 +0530

    c code for variable size

---
+++ 1_variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("size of char %ld byte\n", sizeof(char));
+    printf("size of int %ld byte\n", sizeof(int));
+    printf("size of float %ld byte\n", sizeof(float));
+    printf("size of short %ld byte\n", sizeof(short));
+    printf("size of long int %ld byte\n", sizeof(long int));
+    printf("size of double %ld byte\n", sizeof(double));   
+    printf("size of sizeof() %ld byte\n", sizeof(sizeof(int)));
+
+    return 0;
+}
